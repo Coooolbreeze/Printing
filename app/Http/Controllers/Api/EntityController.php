6d5868247291dc0aaec7cbf990e130c551a5c61b
Coooolbreeze <392113643@qq.com>
2018-09-07 14:55:13 +0800

    财务统计

--- app/Http/Controllers/Api/EntityController.php
+++ app/Http/Controllers/Api/EntityController.php
@@ -31,7 +31,7 @@ class EntityController extends ApiController
     {
         if ($request->value) {
             $entities = EntityResource::collection(
-                Entity::where('name', $request->value)->get()
+                Entity::where('name', '%' . $request->value . '%')->get()
             )->show(['id', 'name']);
         } else {
             $entities = Type::with('entities')->get(['id', 'name']);
